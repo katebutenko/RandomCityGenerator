7ed81d40c92f69d67a44e8bd5daa26d6eef92ebf
Ekaterina Butenko <ekb@sitecore.net>
2017-01-20 23:06:24 +0100

    First commit

---
+++ City.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RealAddressing
+{
+  public class City
+  {
+    public string Name;
+    public float Latitude;
+    public float Longitude;
+    public string Country;
+
+    public City()
+    {
+
+    }
+
+    public City(string[] values)
+    {
+      if (values.Count() < 4) return;
+
+      Name = values[0];
+
+      Latitude = float.Parse(values[1], CultureInfo.InvariantCulture.NumberFormat);
+      Longitude = float.Parse(values[2], CultureInfo.InvariantCulture.NumberFormat);
+      try
+      {
+        RegionInfo regionInfo = new RegionInfo(values[3]);
+        Country = regionInfo.DisplayName;
+      }
+      catch
+      {
+        Country = values[3];
+      }
+    }
+  }
+}
---
+++ CityGenerator.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RealAddressing
+{
+  public static class CityGenerator
+  {
+    static List<City> cityCollection;
+    static Random randomizer;
+
+    static CityGenerator()
+    {
+      cityCollection = new List<City>();
+      try
+      {
+        var assembly = Assembly.GetExecutingAssembly();
+        System.IO.StreamReader fileWithCityNames = new StreamReader(assembly.GetManifestResourceStream("RandomCityGenerator.Properties.Resources.resources"));
+        while (!fileWithCityNames.EndOfStream)
+        {
+          string line = fileWithCityNames.ReadLine();
+
+          cityCollection.Add(new City(line.Split('\t')));
+        }
+      }
+      catch (Exception e)
+      {
+        throw e;
+      }
+
+      randomizer = new Random();
+    }
+
+    public static City GetRandomCity()
+    {
+      int randomNumber = randomizer.Next(TotalAmountOfCities);
+      return cityCollection[randomNumber];
+    }
+
+    public static int TotalAmountOfCities
+    {
+      get
+      {
+        return cityCollection.Count();
+      }
+    }
+  }
+}
